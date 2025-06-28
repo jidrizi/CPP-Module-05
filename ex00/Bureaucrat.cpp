f6e68a1fc48958bcd6c8a82630ca04963b66a58b
jidrizi <idrizijuxhin@gmail.com>
2025-06-28 15:05:13 +0200

    not every function is finished but most should be done

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -6,8 +6,54 @@
 /*   By: jidrizi <jidrizi@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 14:33:59 by jidrizi           #+#    #+#             */
-/*   Updated: 2025/06/28 14:34:09 by jidrizi          ###   ########.fr       */
+/*   Updated: 2025/06/28 15:04:27 by jidrizi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : _name("default"), _grade(150)
+{
+	std::cout << "[Default constructor called]";
+}
+
+//prot constr
+
+Bureaucrat::(const Bureaucrat &other)
+{
+	std::cout << "[Copy constructor called]";
+	this->name = other.name;
+	this->grade = other.grade;
+}
+
+Bureaucrat &Bureaucrat:operator=(const Bureaucrat &other)
+{
+	std::cout << "[Copy assigment called]"
+	if (this != other)
+	{
+		this->name = other.name;
+		this->grade = other.grade;
+	}
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "[Destructor called]";
+}
+
+std::string	Bureaucrat::getName() const
+{
+	return (this->name);
+}
+
+int	Bureaucrat::getGrade() const
+{
+	return (this->grade);
+}
+
+std::ostream &operator<<(std::ostream &os, Bureaucrat const &other)
+{
+	os << other.getName() << ", bureaucrat grade " << other.getGrade() << std::endl;
+	return (os);
+}
