ebd756485120c3cdf2ed305b2be51c0c0cbf419a
jidrizi <idrizijuxhin@gmail.com>
2025-08-08 19:34:25 +0200

    just started ex00

---
+++ ex00/includes/Bureaucrat.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jidrizi <jidrizi@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/08 18:08:39 by jidrizi           #+#    #+#             */
+/*   Updated: 2025/08/08 18:28:24 by jidrizi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <string>
+
+class	Bureaucrat
+{
+	private:
+		const std::string	name;
+		int					grade;
+
+	public:
+		//constructors
+		Bureaucrat();
+		Bureaucrat(const std::string name, int grade);
+		Bureaucrat& operator=(const Bureaucrat other);
+		Bureaucrat(const Bureaucrat &other);
+		~Bureaucrat();
+		
+		//functions
+		const std::string	getName(Bureaucrat buro);
+		int					getGrade(Bureaucrat buro);
+};
+
+
+
+
+
+#endif
