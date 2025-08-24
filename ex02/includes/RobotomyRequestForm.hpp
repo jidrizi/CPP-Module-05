98298736c31ee0bdd96457a59ec9aa3b657e6ad1
jidrizi <idrizijuxhin@gmail.com>
2025-08-25 00:23:46 +0200

    started robotomy form

--- ex02/includes/RobotomyRequestForm.hpp
+++ ex02/includes/RobotomyRequestForm.hpp
@@ -6,7 +6,7 @@
 /*   By: jidrizi <jidrizi@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/24 05:42:51 by jidrizi           #+#    #+#             */
-/*   Updated: 2025/08/24 05:43:44 by jidrizi          ###   ########.fr       */
+/*   Updated: 2025/08/25 00:20:21 by jidrizi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,21 @@
 # define ROBOTOMYREQUESTFORM_HPP
 
 # include "AForm.hpp"
+
+class RobotomyRequestForm : public AForm
+{
+	private:
+		std::string	target;
+	
+	public:
+	// constructors
+		RobotomyRequestForm(std::string target);
+		RobotomyRequestForm(RobotomyRequestForm& source);
+		RobotomyRequestForm&	operator=(RobotomyRequestForm& source);
+		~RobotomyRequestForm();
+
+	// functions
+		void			subFormAction() const;
+};
+
+#endif
